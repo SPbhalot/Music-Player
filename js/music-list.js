cdc3d3121184a296d8eb9714c86dd4cca7b71158
SPbhalot <93925594+SPbhalot@users.noreply.github.com>
2022-11-15 21:40:41 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,45 @@
+let allMusic = [
+  {
+    name: "Faded",
+    artist: "Alen Walker",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Shape Of You",
+    artist: "Ed sheeran",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Girls Like You",
+    artist: "Maroon 5",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Memories",
+    artist: "Maroon 5",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "On My Way",
+    artist: "Alen Walker",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Closer",
+    artist: "The Chainsmokers",
+    img: "music-6",
+    src: "music-6",
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
